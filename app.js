09087169bbb86e8e6300bc46750fe47901ce4a2d
Munoz-CSG <jorgemunoz.csg@gmail.com>
2025-02-01 15:25:03 -0300

    Implementación función agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre válido.");
+        return;
+    }
+
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
